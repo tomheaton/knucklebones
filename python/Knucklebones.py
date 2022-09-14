01af103be7397f51a8dcf0d10aad0e3466766834
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-09-15 00:16:46 +0100

    dice placing done

--- python/Knucklebones.py
+++ python/Knucklebones.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import List
 from random import randint
 
@@ -13,50 +15,68 @@ class Knucklebones():
     
     def start(self) -> None:
         print("starting game...")
-        
+        print(f"player {self.starting_player + 1} starts.")
+
         while not self.game_over:
             if self.is_board_full():
                 self.game_over = True
     
             self.display();
-            print(f"player {self.starting_player + 1} starts.")
             roll = self.get_roll()
             
             while True:
+                if self.is_board_full():
+                    self.game_over = True
+                    break
+
                 column = input(f"player {self.current_player + 1}: where do you want to put your {roll}? ")
                 if column.isdigit() and int(column) in [1,2,3]:
-                    break
-                
-            column_index = int(column) - 1
+                    column_index = int(column) - 1
+            
+                    if self.current_player == 0:
+                        if 0 in self.player_one_board[column_index]:
+                            free_row = self.player_one_board[column_index].index(0) 
+                            self.player_one_board[column_index][free_row] = roll
+                            break
+                        else:
+                            print("column is full")
 
-            # TODO: place roll in column
-            if self.current_player == 0:
-                free_row = self.player_one_board[column_index][::-1].index(0)
-                self.player_one_board[column_index][free_row] = roll
-
-            if self.current_player == 1:
-                free_row = self.player_two_board[column_index][::-1].index(0)
-                self.player_two_board[column_index][free_row] = roll
+                    if self.current_player == 1:
+                        if 0 in self.player_two_board[column_index]:
+                            free_row = self.player_two_board[column_index].index(0)
+                            self.player_two_board[column_index][free_row] = roll
+                            break
+                        else:
+                            print("column is full")
+                else:
+                    print("invalid column")
 
             self.current_player = 1 if self.current_player == 0 else 0
             
+            self.clear()
+
         print("game over")
 
     def is_board_full(self) -> bool:
         b1 = self.player_one_board
         b2 = self.player_two_board
+        
+        print(f"{b1[0] + b1[1] + b1[2]}")
 
-        if 0 in (b1[0] + b1[1] + b1[2]):
+        if 0 in (b1[0] + b1[1] + b1[2] + b2[0] + b2[1] + b2[2]):
+            print("boards not full")
             return False
         
-        if 0 in (b2[0] + b2[1] + b2[2]):
-            return False
-            
+        print("boards full")
         return True
         
     def get_roll(self) -> int:
         return randint(1, 6)
-        
+    
+    @staticmethod
+    def clear() -> None:
+        os.system("cls" if os.name == "nt" else "clear")
+
     def display(self) -> None:
         b1 = self.player_one_board
         b2 = self.player_two_board
