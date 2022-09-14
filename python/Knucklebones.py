fadb43d57f3ae66901dd9397ceed82288a65f281
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-09-14 23:59:17 +0100

    code

--- python/Knucklebones.py
+++ python/Knucklebones.py
@@ -23,13 +23,22 @@ class Knucklebones():
             roll = self.get_roll()
             
             while True:
-                column = int(input(f"player {self.current_player + 1}: where do you want to put your {roll}"))
-                if column in [1,2,3]:
+                column = input(f"player {self.current_player + 1}: where do you want to put your {roll}? ")
+                if column.isdigit() and int(column) in [1,2,3]:
                     break
                 
+            column_index = int(column) - 1
+
             # TODO: place roll in column
-            
-            print(f"{column = }")
+            if self.current_player == 0:
+                free_row = self.player_one_board[column_index][::-1].index(0)
+                self.player_one_board[column_index][free_row] = roll
+
+            if self.current_player == 1:
+                free_row = self.player_two_board[column_index][::-1].index(0)
+                self.player_two_board[column_index][free_row] = roll
+
+            self.current_player = 1 if self.current_player == 0 else 0
             
         print("game over")
 
@@ -52,6 +61,8 @@ class Knucklebones():
         b1 = self.player_one_board
         b2 = self.player_two_board
         
+        print()
+
         print("Player One:")
         print(f"|{b1[0][0]}| |{b1[1][0]}| |{b1[2][0]}|")
         print(f"|{b1[0][1]}| |{b1[1][1]}| |{b1[2][1]}|")
