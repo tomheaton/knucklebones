16172f6bfdf14479ab3d14bbe50e1bcbdb336897
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-09-19 14:36:37 +0100

    start rust version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
+
 fn main() {
     println!("knucklebones");
+
+    loop {
+        println!("Please enter chosen column: ");
+  
+        let mut column_string = String::new();
+        io::stdin().read_line(&mut column_string).unwrap();
+        
+        let column: i32 = column_string.parse().unwrap(); 
+
+        println!("Chosen column: {}", column);
+    }
 }
